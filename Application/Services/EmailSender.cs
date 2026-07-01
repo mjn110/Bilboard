a97cd01ea577fa1334d80381c2206fd35c92e17f
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-07-01 15:46:29 +0100

    Adding constructor

--- Application/Services/EmailSender.cs
+++ Application/Services/EmailSender.cs
@@ -7,6 +7,11 @@ namespace Application.Services
 {
     public class EmailSender : IEmailSender
     {
+        private readonly HttpClient _http;
+        public EmailSender(HttpClient http)
+        {
+            _http = http;
+        }
         public async Task SendEmailAsync(string toEmail)
         {
             Console.WriteLine($"Sending email to: {toEmail}");
