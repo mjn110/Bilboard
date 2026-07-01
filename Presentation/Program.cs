62b859197df06f2640e3992cbdbe37fc96e7a6f1
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-07-01 15:51:31 +0100

    add program.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -19,6 +19,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
 builder.Services.AddScoped<IBoardService, BoardService>();
 builder.Services.AddScoped<IConsoleService, ConsoleService>();
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
