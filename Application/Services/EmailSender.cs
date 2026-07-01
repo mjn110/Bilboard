d6d41ba14ddccc314e55c1c8c6582e7b5f9e3362
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-07-01 15:57:15 +0100

    Add email code to the service

--- Application/Services/EmailSender.cs
+++ Application/Services/EmailSender.cs
@@ -8,13 +8,36 @@ namespace Application.Services
     public class EmailSender : IEmailSender
     {
         private readonly HttpClient _http;
-        public EmailSender(HttpClient http)
+        private readonly string _apiKey;
+        public EmailSender(HttpClient http, string apiKey)
         {
             _http = http;
+            _apiKey = apiKey;
         }
         public async Task SendEmailAsync(string toEmail)
         {
             Console.WriteLine($"Sending email to: {toEmail}");
+
+            var payload = new
+            {
+                sender = new { email = "info@yourdomain.com", name = "Your App" },
+                to = new[] { new { email = toEmail } },
+                subject = "Bilboard verification email",
+                htmlContent = "Hi this is a verification email for your Bilboard account."
+            };
+
+            var req = new HttpRequestMessage(HttpMethod.Post, "https://api.brevo.com/v3/smtp/email")
+            {
+                Content = new StringContent(
+                    System.Text.Json.JsonSerializer.Serialize(payload),
+                    System.Text.Encoding.UTF8,
+                    "application/json")
+            };
+
+            req.Headers.Add("api-key", _apiKey);
+
+            var res = await _http.SendAsync(req);
+            res.EnsureSuccessStatusCode();
         }
     }
 }
