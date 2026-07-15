d64c11923cbba2d9b593a400a7a614ee4fa8b3c3
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-07-15 17:16:50 +0100

    Required removed

--- Bilboard/ViewModels/ResetViewModel.cs
+++ Bilboard/ViewModels/ResetViewModel.cs
@@ -4,11 +4,9 @@ namespace Bilboard.ViewModels
 {
     public class ResetViewModel
     {
-        [Required]
         [EmailAddress]
-        public string Email { get; set; }
-        [Required]
-        public string Token { get; set; }
+        public string? Email { get; set; }
+        public string? Token { get; set; }
         [Required]
         public string Password { get; set; }
         [Required]
