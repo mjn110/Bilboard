16f6aa0932b9279c29cccaa711f7b5112a4b093d
mjn110 <57441281+mjn110@users.noreply.github.com>
2026-08-21 16:33:23 +0100

    Added Board, Component, and Value models in Infra
    In this commit I added data architecture for storing boards and its components in database.

---
+++ Infrastructure/Model/Component.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Infrastructure.Model
+{
+    public class Component
+    {
+        [Key]
+        public string ComponentId { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public Board Board { get; set; }
+        [ForeignKey("Board")]
+        public string BoardId { get; set; }
+    }
+}
---
+++ Infrastructure/Model/Value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Infrastructure.Model
+{
+    public class Value
+    {
+        [Key]
+        public string ValueId { get; set; }
+        [Required]
+        public string Value { get; set; }
+        public Component Component { get; set; }
+        [ForeignKey("Component")]
+        public string ComponentId { get; set; }
+    }
+}
